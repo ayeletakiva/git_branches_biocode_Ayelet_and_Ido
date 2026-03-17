c29dedfb3d07b45410b58c25046965eb648c1374
levyidodo1995@gmail.com <levyidodo1995@gmail.com>
2026-03-17 11:57:12 +0200

    קוד לחישוב AT CONTENT

--- scripts/fasta_analysis.py
+++ scripts/fasta_analysis.py
@@ -1,10 +1,6 @@
-def read_fasta(filename):
-    seq = ""
-    with open(filename) as f:
-        for line in f:
-            if not line.startswith(">"):
-                seq += line.strip()
-    return seq
-
-sequence = read_fasta("data/sequence.fasta")
-print(sequence)
+def at_content(seq):
+    a = seq.count("A")
+    t = seq.count("T")
+    gc = (a + t) / len(seq) * 100
+    return at
+print(" AT content:", at_content(sequence))
